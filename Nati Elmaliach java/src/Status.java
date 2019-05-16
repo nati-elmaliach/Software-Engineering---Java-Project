217c037443b980dddc923a80c153338b90011070
Owner-PC\Owner <natielmaliach3197@gmail.com>
2019-05-16 12:39:44 +0300

    Nati

---
+++ Nati Elmaliach java/src/Status.java
@@ -0,0 +1,15 @@
+public class Status {
+    private String status;
+
+    public Status(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
