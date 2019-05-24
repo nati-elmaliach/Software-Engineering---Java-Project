9626b10f6df7cdc81ccc318ae66a8018f666d91b
lizer21 <lizer21@gmail.com>
2019-05-24 16:12:07 +0300

    CarsShop
    new folder

---
+++ CarsShop/src/Leizy/Status.java
@@ -0,0 +1,17 @@
+package Leizy;
+
+public class Status {
+    private String status;
+
+    public Status(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
